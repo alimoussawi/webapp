918ee1663f538619f3a3b7d25dc49c971f58fdb1
ali moussawi <aloush.aam@gmail.com>
2017-12-09 11:54:09 +0200

    Add substraction

--- ostpc-essentials/src/main/java/ua/nure/ostpc/SimpleMaths.java
+++ ostpc-essentials/src/main/java/ua/nure/ostpc/SimpleMaths.java
@@ -1,7 +1,7 @@
 package ua.nure.ostpc;
 
 public class SimpleMaths {
-    public int calculations(int a, int b){
-        return a + b;
+    public int subtract(int a, int b){
+        return a - b;
     }
 }
