d10f3f8f8c1def4ca34fec3803630d1353e8c027
Eugene Kovaljov <ekovaljov@avertra.com>
2017-12-16 12:31:24 +0200

    Add task

---
+++ ostpc-essentials/src/main/java/ua/nure/ostpc/second/lesson/PracticeTask.java
@@ -0,0 +1,32 @@
+package ua.nure.ostpc.second.lesson;
+
+public class PracticeTask {
+    /**
+     * 1. Init variables with following types:
+     * Short,
+     * Char,
+     * Int,
+     * Double
+     */
+
+    /*
+       2. Convert char to int
+       int to double
+       short max value + short max value to int
+       and to double
+     */
+
+    /*
+        show the hierarchy of automatical
+        cast of data types and explain why it works
+     */
+
+    /*
+    Use the same hierarchy for boxed values (Number, Character, Integer etc)
+    And explain how boxing works
+     */
+
+    /*
+    Show the ability to import static members of any library (Math for example)
+     */
+}
