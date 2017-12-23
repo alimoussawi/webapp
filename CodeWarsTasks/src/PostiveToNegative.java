ef6af83e0699f563c345cb2d24d55c1dffe8d506
Eugene Kovaljov <didevgen@gmail.com>
2017-12-23 10:06:02 +0200

    Revert "some codewars tasks"
    This reverts commit 78915b6a4447409a758d86fd50de9f3aeefc4f71.

--- CodeWarsTasks/src/PostiveToNegative.java
+++
@@ -1,8 +0,0 @@
-public class PostiveToNegative {
-    public static int makeNegative(final int x) {
-        if (x>0)
-            return -x;
-        else
-            return x;
-    }
-}
--- CodeWarsTasks/src/ReverseString.java
+++
@@ -1,7 +0,0 @@
-public class ReverseString{
-        public static String solution(String str) {
-            // Your code here...
-            return new StringBuilder(str).reverse().toString();
-        }
-// i got the method that reverse a String from google  :p
-}
