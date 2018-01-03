83778ac7bb6824e889c325fb23c69e3e3a6a4d1e
ali moussawi <aloush.aam@gmail.com>
2018-01-03 19:51:29 +0200

    my market task

---
+++ ostpc-essentials/src/main/java/ua/nure/ostpc/MyTask/Goods.java
@@ -0,0 +1,14 @@
+package ua.nure.ostpc.MyTask;
+
+public class Goods extends MyMarket {
+    public Goods(String goodname ,int price,String provider) {
+this.setGoodName(goodname);
+this.setPrice(price);
+this.setProvider(provider);
+
+
+
+    }
+
+
+}
